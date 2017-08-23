9389753f718e3b4a91edb9f0143fa21124dce359
ehime <dodomeki@gmail.com>
2017-08-23 10:40:25 -0700

    adding berksfile

---
+++ Berksfile
@@ -0,0 +1,5 @@
+site :opscode
+
+cookbook 'apt'
+cookbook 'docker',  github: "bflad/chef-docker"
+cookbook "mesos",   github: "ehime/cookbook-mesos"
--- scripts/aws/install-vbguest-addts.sh
+++ scripts/aws/install-vbguest-addts.sh
@@ -2,7 +2,9 @@
 
 mkdir /tmp/isomount
 mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/isomount
+
 /tmp/isomount/VBoxLinuxAdditions.run install
+
 umount /tmp/isomount
 rm -rf /tmp/isomount
 
